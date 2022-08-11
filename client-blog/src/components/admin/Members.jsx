e2b61fcb6da3547ce0654e3f3b8b6555a7583db7
elis-11 <eliza.arzanukaeva@gmail.com>
2022-08-11 10:44:02 +0200

    Start Blog

---
+++ client-blog/src/components/admin/Members.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDataContext } from "../../context/DataProvider";
+import { getUsersApi } from "../../helpers/apiCalls";
+import {UserList} from "../UserList"
+import "./Admin.scss";
+
+export const Members = () => {
+  const navigate = useNavigate();
+
+  const { user, users, setUsers, errors, setErrors } = useDataContext();
+
+  useEffect(() => {
+    if (!user) {
+      return navigate("/login");
+    }
+    const loadData = async () => {
+      console.log(user);
+      const result = await getUsersApi(user.token);
+
+      if (result.error) {
+        return setErrors(result.error);
+      }
+      setErrors("");
+      setUsers(result);
+    };
+    console.log(user);
+
+    loadData();
+  }, [user]);
+
+  return (
+    <div className="Members">
+      <h2>Members</h2>
+      <UserList />
+      <div className="errors">{errors}</div>
+    </div>
+  );
+};
